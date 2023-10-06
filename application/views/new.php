8d6dde4e4b26976a88158a1c1021318f1a180cfc
rorjuliancho <julian.mora95@gmail.com>
2023-10-06 16:33:13 -0500

    Registro Paciente y Padre de mascota

--- application/views/new.php
+++ application/views/new.php
@@ -1,8 +1,130 @@
  <!-- Begin Page Content -->
  <div class="container-fluid">
+     <div class="card">
+         <h2>Registrar nuevo paciente</h2>
+         <div class="card-body">
+             <form action="">
+                 <form>
+                     <div class="form-row">
+                         <div class="form-group col-md-6">
+                             <label for="inputEmail4">Nombre del canino</label>
+                             <input type="text" class="form-control" id="inputEmail4" placeholder="Rex">
+                         </div>
+                         <div class="form-group col-md-6">
+                             <label for="inputPassword4">Sexo</label>
+                             <select class="form-control">
+                                 <option value="">Seleccionar</option>
+                                 <option value="">Hembra</option>
+                                 <option value="">Macho</option>
+                             </select>
+                         </div>
+                     </div>
 
-     <!-- Page Heading -->
-     <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
 
+                     <div class="form-row">
+                         <div class="form-group col-md-6">
+                             <label for="">Edad</label>
+                             <input type="text" class="form-control" id="" placeholder="3 meses / años">
+                         </div>
+                         <div class="form-group col-md-6">
+                             <label for="">Raza</label>
+                             <input type="text" class="form-control" id="" placeholder="Pastor Aleman">
+                         </div>
+                     </div>
+
+                     <div class="form-row">
+                         <div class="form-group col-md-6">
+                             <label for="">Tamaño</label>
+                             <select class="form-control">
+                                 <option value="">Seleccionar</option>
+                                 <option value="">Pequeño</option>
+                                 <option value="">Mediano</option>
+                                 <option value="">Grande</option>
+                             </select>
+                         </div>
+                         <div class="form-group col-md-6">
+                             <label for="">Peso</label>
+                             <input type="text" class="form-control" id="" placeholder="Pastor Aleman">
+                         </div>
+                     </div>
+
+                     <div class="form-row">
+                         <div class="form-group col-md-4">
+                             <label for="">Ancho de la Cadera (cm)</label>
+                             <input type="text" class="form-control" id="" placeholder="20">
+                         </div>
+                         <div class="form-group col-md-4">
+                             <label for="">Altura del Fémur al suelo (cm)</label>
+                             <input type="text" class="form-control" id="" placeholder="10">
+                         </div>
+                         <div class="form-group col-md-4">
+                             <label for="">Altura de la ingle hasta el suelo (cm)</label>
+                             <input type="text" class="form-control" id="" placeholder="20">
+                         </div>
+                     </div>
+
+                     <div class="form-row">
+                         <div class="form-group col-md-4">
+                             <label for="">Longitud del hombro hasta la cabeza del hombro (cm)</label>
+                             <input type="text" class="form-control" id="" placeholder="20">
+                         </div>
+                         <div class="form-group col-md-4">
+                             <label for="">Circunferencia del pecho (cm)</label>
+                             <input type="text" class="form-control" id="" placeholder="10">
+                         </div>
+                         <div class="form-group col-md-4">
+                             <label for="">Circunferencia del Cuello (cm)</label>
+                             <input type="text" class="form-control" id="" placeholder="20">
+                         </div>
+                     </div>
+
+                     <div class="form-row">
+                         <div class="form-group col-md-12">
+                             <textarea class="form-control" name="" id="" cols="30" rows="3">Observaciones</textarea>
+                         </div>
+                     </div>
+
+                     <h3>Registro Padre de Mascota</h3>
+
+
+                     <div class="form-row">
+                         <div class="form-group col-md-6">
+                             <label for="inputEmail4">Nombre</label>
+                             <input type="text" class="form-control" id="inputEmail4" placeholder="Daniela">
+                         </div>
+                         <div class="form-group col-md-6">
+                             <label for="inputEmail4">Apellido</label>
+                             <input type="text" class="form-control" id="inputEmail4" placeholder="Polanco">
+                         </div>
+                     </div>
+
+                     <div class="form-row">
+                         <div class="form-group col-md-6">
+                             <label for="inputEmail4">Dirección</label>
+                             <input type="text" class="form-control" id="inputEmail4" placeholder="Cll 76 # 80-40">
+                         </div>
+                         <div class="form-group col-md-6">
+                             <label for="inputEmail4">Teléfono</label>
+                             <input type="text" class="form-control" id="inputEmail4" placeholder="3201478520">
+                         </div>
+                     </div>
+
+                     <div class="form-row">
+                         <div class="form-group col-md-6">
+                             <label for="inputEmail4">Email</label>
+                             <input type="email" class="form-control" id="inputEmail4" placeholder="daniela@pruebas.com">
+                         </div>
+                         <div class="form-group col-md-6">
+                             <label for="inputEmail4">Cédula</label>
+                             <input type="text" class="form-control" id="inputEmail4" placeholder="10102580">
+                         </div>
+                     </div>
+                     <center>
+                         <button type="submit" class="btn btn-primary">Registrar</button>
+                     </center>
+                 </form>
+             </form>
+         </div>
+     </div>
  </div>
  <!-- /.container-fluid -->
