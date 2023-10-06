d8a6bf6bd445ce8f49f26e3fb133e7b5b28545f2
rorjuliancho <julian.mora95@gmail.com>
2023-10-06 17:54:58 -0500

    ¿Cómo medir?

--- application/views/main/head.php
+++ application/views/main/head.php
@@ -85,7 +85,7 @@
 
             <!-- Nav Item - Como medir -->
             <li class="nav-item ">
-                <a class="nav-link" href="tables.html">
+                <a class="nav-link" href="<?= base_url('Admin/medidas') ?>">
                     <i class="fas fa-fw fa-table"></i>
                     <span>¿Cómo Medir ?</span></a>
             </li>
